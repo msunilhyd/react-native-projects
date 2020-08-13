2874a59985e8fe901e57461dd23e527b98edf5a9
msunilhyd <msunilhyd@gmail.com>
2020-08-13 15:52:01 +0530

    13-Aug-20 : reactnative compnents simple

---
+++ one/ComponentImport.js
@@ -0,0 +1,46 @@
+import React, { Component } from 'react'
+import {
+	Text,
+	View
+} from 'react-native'
+
+class Home extends Component {
+	render() {
+		return (
+			<View>
+				<Header />
+				<Main />
+				<Footer />
+			</View>
+		)
+	}
+}
+
+class Header extends Component {
+	render(){
+		return <View> 
+			<Text>HEADER</Text>
+		</View>
+	}
+}
+
+const Header = () => (
+
+	<View>
+		<Text>HEADER</Text>
+	</View>
+)
+
+
+const Footer = () => (
+	<View>
+		<Text>Footer</Text>
+	</View>
+)
+
+
+const Main = () => (
+	<View>
+		<Text> Main </Text>
+	</View>
+)
